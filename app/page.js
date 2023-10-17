46420b95bf18302aa75510882db87c889ac9093e
mohammad <m.dousi96@gmail.com>
2023-10-17 15:16:51 +0330

    create list 2

--- app/page.js
+++ app/page.js
@@ -32,7 +32,6 @@ export default function Home() {
         </div>
 
         {/* player */}
-
         <div className="w-full h-1/2 p-2 flex flex-col justify-start items-center gap-6 rounded-xl">
           <img
             src="/cover.jpg"
@@ -71,7 +70,7 @@ export default function Home() {
       </div>
 
       {/*  main contanier  */}
-      <div className="w-4/5 h-full px-3 flex flex-col justify-start items-center gap-6 overflow-hidden">
+      <div className="w-4/5 h-full px-3 flex flex-col justify-start items-center gap-6 overflow-auto">
         {/*  search box and profile  */}
         <div className="w-full py-2 flex flex-row justify-between items-center gap-6">
           {/* profile */}
@@ -82,9 +81,14 @@ export default function Home() {
                 alt="/profile2.png"
                 className="h-full shadow-xl rounded-lg object-cover"
               />
-              <p className="bg-transparent text-sm text-gray-400">Danil Moyu</p>
 
-              <i className="fa fa-angle-down text-sm text-gray-400"></i>
+              <div className="w-full flex flex-row justify-between items-center pr-4">
+                <p className="bg-transparent text-sm text-gray-400 font-bold">
+                  Danil Moyu
+                </p>
+
+                <i className="fa fa-angle-down text-sm text-gray-400"></i>
+              </div>
             </div>
             <i className="fa fa-bell text-gray-400"></i>
           </div>
@@ -101,26 +105,25 @@ export default function Home() {
         </div>
 
         {/* banner */}
-        <div className="w-full h-40 flex flex-row justify-start items-start gap-6 rounded-xl shadow-lg overflow-hidden">
-          <div className="w-full h-40 rounded-xl shadow-lg overflow-hidden">
+        {/* <div className="w-full flex flex-row justify-start items-start gap-6 rounded-xl shadow-lg">
+          <div className="w-full rounded-xl shadow-lg overflow-hidden">
             <img
               src="/banner.jpg"
               alt="/banner.png"
-              className="w-full h-full object-cover"
+              className="w-full h-36 object-cover"
             />
           </div>
 
-          <div className="w-full h-40 rounded-xl shadow-lg overflow-hidden">
+          <div className="w-full rounded-xl shadow-lg overflow-hidden">
             <img
-              src="/banner.jpg"
-              alt="/banner.png"
-              className="w-full h-full object-cover"
+              src="/banner2.jpg"
+              alt="/banner2.png"
+              className="w-full h-36 object-cover"
             />
           </div>
-        </div>
+        </div> */}
 
-        {/* fav artists */}
-
+        {/* favorite artists */}
         <section className="w-full flex flex-col justify-start items-start gap-3">
           <div className="w-full flex flex-row justify-between items-center py-2 border-b border-slate-700">
             <h1 className="text-white/70 text-lg capitalize">
@@ -132,92 +135,163 @@ export default function Home() {
             </h1>
           </div>
 
-          <section className="w-full flex flex-row justify-start items-start gap-6">
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+          <section className="w-full flex flex-row justify-start items-start gap-5">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile1.png"
                 alt="/profile1.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 drake
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile2.png"
                 alt="/profile2.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 doja cat
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile5.png"
                 alt="/profile5.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 dua lipa
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
-                src="/profile7.png"
-                alt="/profile7.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                src="/profile4.png"
+                alt="/profile4.png"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 lizzo
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
-                src="/profile4.png"
-                alt="/profile4.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                src="/profile7.png"
+                alt="/profile7.png"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 harry styles
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile8.png"
                 alt="/profile8.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 ava max
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile6.png"
                 alt="/profile6.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 mikle jakson
               </h2>
             </div>
 
-            <div className="px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
               <img
                 src="/profile5.png"
                 alt="/profile5.png"
-                className="w-24 h-24 object-cover rounded-full shadow-xl"
+                className="h-28 object-cover rounded-full shadow-xl"
               />
               <h2 className="text-white/40 text-xs font-normal capitalize ">
                 dua lipa
+              </h2>
+            </div>
+
+            <div className="p-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+              <img
+                src="/profile2.png"
+                alt="/profile2.png"
+                className="h-28 object-cover rounded-full shadow-xl"
+              />
+              <h2 className="text-white/40 text-xs font-normal capitalize ">
+                dua lipa
+              </h2>
+            </div>
+          </section>
+        </section>
+
+        {/* recommended for you */}
+
+        <section className="w-full flex flex-col justify-start items-start gap-3">
+          <div className="w-full flex flex-row justify-between items-center py-2 border-b border-slate-700">
+            <h1 className="text-white/70 text-lg capitalize">
+              recommended for you
+            </h1>
+
+            <h1 className="text-white/40 text-xs capitalize rounded-full cursor-pointer duration-300 hover:text-cyan-500">
+              see all
+            </h1>
+          </div>
+
+          <section className="w-full flex flex-row justify-start items-start gap-6">
+            <div className="w-1/4 px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+              <img
+                src="/banner.jpg"
+                alt="/banner.jpg"
+                className="w-full h-32 object-cover rounded-xl"
+              />
+              <h2 className="w-full py-1 text-white/70 text-sm font-normal capitalize">
+                my favorite songs
+              </h2>
+            </div>
+
+            <div className="w-1/4 px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+              <img
+                src="/banner2.jpg"
+                alt="/banner2.jpg"
+                className="w-full h-32 object-cover rounded-xl"
+              />
+              <h2 className="w-full py-1 text-white/70 text-sm font-normal capitalize">
+                house music festival
+              </h2>
+            </div>
+
+            <div className="w-1/4 px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+              <img
+                src="/banner.jpg"
+                alt="/banner.jpg"
+                className="w-full h-32 object-cover rounded-xl"
+              />
+              <h2 className="w-full py-1 text-white/70 text-sm font-normal capitalize">
+                my favorite songs
+              </h2>
+            </div>
+
+            <div className="w-1/4 px-3 py-2 flex flex-col justify-center items-center gap-2 bg-slate-950/50 rounded-2xl">
+              <img
+                src="/banner2.jpg"
+                alt="/banner2.jpg"
+                className="w-full h-32 object-cover rounded-xl"
+              />
+              <h2 className="w-full py-1 text-white/70 text-sm font-normal capitalize">
+                house music festival
               </h2>
             </div>
           </section>
